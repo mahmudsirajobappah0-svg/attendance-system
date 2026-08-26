80cb7383bf521a7a52d3065361a951f1ab01c387
mahmudsirajobappah0-svg <mahmudsirajobappah0@gmail.com>
2026-08-26 03:15:34 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implimentation("androidx.navigation:navigation-compose:2.8.9")
 
     // Jetpack Compose
     implementation(platform("androidx.compose:compose-bom:2024.06.00"))
