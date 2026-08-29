1dfb86f34cde3f56c4fb47eb7f1912a1a6dc5a12
mahmudsirajobappah0-svg <mahmudsirajobappah0@gmail.com>
2026-08-29 20:34:07 +0300

    Update gpsverifier.kt

--- app/src/main/java/com/example/attendancesystem2/gpsverifier.kt
+++ app/src/main/java/com/example/attendancesystem2/gpsverifier.kt
@@ -1,2 +1,34 @@
 package com.example.attendancesystem2
 
+import android.location.Location
+
+object GPSVerifier {
+
+    fun isWithinAllowedDistance(
+
+        studentLatitude: Double,
+        studentLongitude: Double,
+
+        lecturerLatitude: Double,
+        lecturerLongitude: Double,
+
+        allowedDistanceMeters: Float = 100f
+
+    ): Boolean {
+
+        val result = FloatArray(1)
+
+        Location.distanceBetween(
+
+            studentLatitude,
+            studentLongitude,
+
+            lecturerLatitude,
+            lecturerLongitude,
+
+            result
+        )
+
+        return result[0] <= allowedDistanceMeters
+    }
+}
