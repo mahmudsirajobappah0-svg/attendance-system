28110781568764ec7bd0f7a78441e342af80c7a2
mahmudsirajobappah0-svg <mahmudsirajobappah0@gmail.com>
2026-08-29 20:33:14 +0300

    Update Attendancerepository.kt

--- app/src/main/java/com/example/attendancesystem2/Attendancerepository.kt
+++ app/src/main/java/com/example/attendancesystem2/Attendancerepository.kt
@@ -1,2 +1,47 @@
 package com.example.attendancesystem2
 
+data class AttendanceRecord(
+
+    val studentId: String = "",
+
+    val studentName: String = "",
+
+    val course: String = "",
+
+    val date: String = "",
+
+    val status: String = ""
+)
+
+object AttendanceRepository {
+
+    private val attendanceRecords =
+        mutableListOf<AttendanceRecord>()
+
+    fun markAttendance(
+        record: AttendanceRecord
+    ) {
+
+        attendanceRecords.add(record)
+    }
+
+    fun getAttendance(): List<AttendanceRecord> {
+
+        return attendanceRecords
+    }
+
+    fun getAttendancePercentage(): Int {
+
+        if (attendanceRecords.isEmpty()) {
+            return 0
+        }
+
+        val present =
+            attendanceRecords.count {
+                it.status == "Present"
+            }
+
+        return (present * 100) /
+                attendanceRecords.size
+    }
+}
