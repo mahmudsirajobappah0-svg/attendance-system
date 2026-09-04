ee37f3e2a4403c00d92c36741357698743c12bd0
mahmudsirajobappah0-svg <mahmudsirajobappah0@gmail.com>
2026-09-04 18:05:40 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/attendancesystem2/MainActivity.kt
+++ app/src/main/java/com/example/attendancesystem2/MainActivity.kt
@@ -10,7 +10,7 @@ import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentActivity
 import com.google.firebase.auth.FirebaseAuth
-
+import androidx.activity.compose.setContent
 class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
